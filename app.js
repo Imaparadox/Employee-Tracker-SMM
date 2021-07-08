de5aff63522410a345c059fc6bc5c37a362e854a
Imaparadox <stevemarsh117@gmail.com>
2021-07-08 16:20:29 -0400

    Altered schema and seed files, database works correctly now

---
+++ app.js
@@ -0,0 +1,4 @@
+const mysql = require('mysql');
+const inquire = require('inquirer');
+const cTable = require('console.table');
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
-USE employees_db;
+USE employee_db;
 
-INSERT INTO department (name)
-VALUES 
+INSERT INTO department(name)
+VALUES
 ('Sales'),
 ('Engineering'),
 ('Finance'),
@@ -10,23 +10,19 @@ VALUES
 INSERT INTO role (title, salary, department_id)
 VALUES
 ('Sales Lead', 100000, 1),
-('Sales Person', 70000, 1),
-('Lead Engineer', 150000, 2),
-('Software Engineer', 120000, 2),
-('Accountant', 125000, 3),
+('Sales Person', 700000, 1),
+('Lead Engineer', 160000, 2),
+('Software Engineer', 130000, 3),
 ('Legal Team Lead', 250000, 4),
 ('Lawyer', 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
-
-
-
+('James', 'Harvey', 1, NULL),
+('Mike', 'Tyson', 2, NULL),
+('John', 'Smith', 3, NULL),
+('Katie', 'Garvy', 4, 1),
+('Jen', 'Stellar', 6, 2),
+('John', 'Mack', 5, 3),
+('David', 'Lesly', 3, 4),
+('Max', 'Pane', 4, 3);
